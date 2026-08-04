7e242cdfa722fac56fde001fc9649655217a712c
hithaankam <23wh1a1289@bvrithyderabad.edu.in>
2026-08-04 13:50:07 +0530

    include sqllite requirements

--- models.py
+++ models.py
@@ -1,7 +1,29 @@
 from dataclasses import dataclass, field
-
+from sqlalchemy import Column, Integer, String
+from database import Base
 
 @dataclass
 class Result:
     success: bool
     errors: list[str] = field(default_factory=list)
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+
+    username = Column(
+        String(50),
+        unique=True,
+        nullable=False
+    )
+
+    password = Column(
+        String(255),
+        nullable=False
+    )
+
+    role = Column(
+        String(20),
+        nullable=False
+    )
